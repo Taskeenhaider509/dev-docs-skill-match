6f914dbfe59ec74692d2eceeb0719cfa3abe2eee
Taskeenhaider509 <taskeen_haider_intern@netixsol.com>
2024-06-13 18:24:15 +0500

    updated-title

--- dev-docs-skill-match/docusaurus.config.ts
+++ dev-docs-skill-match/docusaurus.config.ts
@@ -3,7 +3,7 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site t',
+  title: 'Skill Match',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
